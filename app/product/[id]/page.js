909ef849590f556aba3329c59fe366b6f92d8e66
job van der ploeg <job.vanderploeg@gmail.com>
2023-08-25 17:49:35 +0200

    small changes

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -1,11 +1,9 @@
 "use client";
-import { useParams } from "next/navigation";
 import { getProduct } from "../../../lib/supabaseAPI";
 import ProductOverview from "../../../components/webshop/productOverview";
 
-export default async function Page() {
-  const { id } = useParams();
-  const product = await getProduct(id);
+export default async function Page({ params }) {
+  const product = await getProduct(params.id);
 
   return (
     <div>
