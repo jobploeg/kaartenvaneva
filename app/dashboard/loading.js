dfa52980e463bf417482099327c841b04f728f14
job van der ploeg <job.vanderploeg@gmail.com>
2023-08-25 14:42:10 +0200

    seperated dashboard from website

--- app/dashboard/loading.js
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold">Loading...</h1>
-      </div>
-    </div>
-  );
-}
--- app/dashboard/page.js
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-      <h1 className="text-4xl m-10">Dashboard</h1>
-    </div>
-  );
-}
